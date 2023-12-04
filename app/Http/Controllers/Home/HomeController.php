d827278a128f6c82d0a80ee28e868f0908a2176c
yacoubdemire <yacoubdemire@gmail.com>
2023-12-04 15:20:43 +0100

    content

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 {
     public function index(){
 
-        $data['products'] = Product::active()->featured()->latest()->take(8)->get();;
+        $data['products'] = Product::orderBy("name","DESC")->active()->featured()->latest()->take(8)->get();
 
         $data['categories'] = Category::parents()->active()->get();
 
