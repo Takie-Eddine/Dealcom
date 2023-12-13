def4576f0a4cdc4d1f1d77d1e95e9eb46afe754d
yacoubdemire <yacoubdemire@gmail.com>
2023-12-13 14:07:48 +0100

    main page

--- resources/views/user/layouts/main.blade.php
+++ resources/views/user/layouts/main.blade.php
@@ -218,7 +218,7 @@
                                             @if ($category->slug == 'apparel')
                                                 <img src="{{asset('assets/logo/clothes.jpg')}}" alt="">
                                             @endif
-                                            @if ($category->slug == 'Furniture')
+                                            @if ($category->slug == 'furniture')
                                                 <img src="{{asset('assets/logo/fourniture.jpg')}}" alt="">
                                             @endif
                                             @if ($category->slug == 'food')
