b6fb6b9860d8e67a48476543692393adb672171e
yacoubdemire <yacoubdemire@gmail.com>
2024-02-26 12:08:28 +0100

    verify user

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -30,57 +30,70 @@
     </div>
 </x-guest-layout> --}}
 
+<!DOCTYPE html>
 
-@extends('admin.auth.layouts.master')
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" direction="{{ app() -> getLocale() === 'ar' ? 'rtl' : 'ltr'}}" dir="{{ app() -> getLocale() === 'ar' ? 'rtl' : 'ltr'}}" style="direction: {{ app() -> getLocale() === 'ar' ? 'rtl' : 'ltr'}}">
+	<head>
+		<title>Verify Email</title>
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" href="{{asset('assets/logo/logo.png')}}" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+		<link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+        @if ( app() -> getLocale() === 'ar')
+            <link href="{{asset('assets/css/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+        @else
+            <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+        @endif
 
-@section('title','Verify Email')
+	</head>
 
-
-@push('style')
-
-@endpush
-
-@section('content')
-
-<div class="d-flex flex-column flex-root" id="kt_app_root">
-    <style>body { background-image: url('{{asset("assets/media/auth/bg5.jpg")}}'); } [data-bs-theme="dark"] body { background-image: url('{{asset("assets/media/auth/bg5-dark.jpg")}}'); }</style>
-    <div class="d-flex flex-column flex-center flex-column-fluid">
-        <div class="d-flex flex-column flex-center text-center p-10">
-            <div class="card card-flush w-lg-650px py-5">
-                <div class="card-body py-15 py-lg-20">
-                    <div class="mb-14">
-                        <a href="" class="">
-                            <img alt="Logo" src="{{asset('assets/media/logos/custom-2.svg')}}" class="h-40px" />
-                        </a>
-                    </div>
-                    <h1 class="fw-bolder text-gray-900 mb-5">{{__('master.verify your email')}}</h1>
-                    <div class="mb-11">
-                        <form method="POST" action="{{ route('verification.send') }}">
-                            @csrf
-
-                            <div>
-                                <button type="submit" class="btn btn-sm btn-primary">
-                                    <span> {{__('master.resend verification email')}} </span>
-                                </button>
+	<body id="kt_body" class="app-blank">
+        <div class="d-flex flex-column flex-root" id="kt_app_root">
+            <style>body { background-image: url('{{asset("assets/media/auth/bg5.jpg")}}'); } [data-bs-theme="dark"] body { background-image: url('{{asset("assets/media/auth/bg5-dark.jpg")}}'); }</style>
+            <div class="d-flex flex-column flex-center flex-column-fluid">
+                <div class="d-flex flex-column flex-center text-center p-10">
+                    <div class="card card-flush w-lg-650px py-5">
+                        <div class="card-body py-15 py-lg-20">
+                            <div class="mb-14">
+                                <a href="" class="">
+                                    <img alt="Logo" src="{{asset('assets/media/logos/custom-2.svg')}}" class="h-40px" />
+                                </a>
                             </div>
-                        </form>
-                        {{-- <a href="../../demo1/dist/index.html" class="btn btn-sm btn-primary">Skip for now</a> --}}
-                    </div>
+                            <h1 class="fw-bolder text-gray-900 mb-5">{{__('master.verify your email')}}</h1>
+                            <div class="mb-11">
+                                <form method="POST" action="{{ route('verification.send') }}">
+                                    @csrf
 
-                    <div class="mb-0">
-                        <img src="{{asset('assets/media/auth/please-verify-your-email.png')}}" class="mw-100 mh-300px theme-light-show" alt="" />
-                        <img src="{{asset('assets/media/auth/please-verify-your-email-dark.png')}}" class="mw-100 mh-300px theme-dark-show" alt="" />
+                                    <div>
+                                        <button type="submit" class="btn btn-sm btn-primary">
+                                            <span> {{__('master.resend verification email')}} </span>
+                                        </button>
+                                    </div>
+                                </form>
+                                {{-- <a href="../../demo1/dist/index.html" class="btn btn-sm btn-primary">Skip for now</a> --}}
+                            </div>
+
+                            <div class="mb-0">
+                                <img src="{{asset('assets/media/auth/please-verify-your-email.png')}}" class="mw-100 mh-300px theme-light-show" alt="" />
+                                <img src="{{asset('assets/media/auth/please-verify-your-email-dark.png')}}" class="mw-100 mh-300px theme-dark-show" alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+        @yield('content')
 
 
-@endsection
+		<script>var hostUrl = "{{asset('assets//')}}";</script>
+
+		<script src="{{asset('assets/plugins/global/plugins.bundle.js/')}}"></script>
+		<script src="{{asset('assets/js/scripts.bundle.js/')}}"></script>
 
 
-@push('script')
 
-@endpush
+	</body>
+
+</html>
+
