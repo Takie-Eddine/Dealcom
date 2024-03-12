5a2c8bc90e4f1db49d2903b6ed9ccce4764c461c
yacoubdemire <yacoubdemire@gmail.com>
2024-03-12 15:00:45 +0100

    notification

--- app/Notifications/ProductRequestNotification.php
+++ app/Notifications/ProductRequestNotification.php
@@ -37,41 +37,41 @@ class ProductRequestNotification extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         $message = new MailMessage ;
-        if ($this->request->datails->name) {
-            $message
-                    ->subject('New Request')
-                    ->greeting("Hello {$notifiable->name},")
-                    ->line('New request for the product ('.$this->request->details->name.') has been created')
-                    ->action('Show Details', url('admin/request/show',$this->request->id))
-                    ->line('Thank you for using our application!');
-        }else{
+        // if ($this->request->datails->name) {
+        //     $message
+        //             ->subject('New Request')
+        //             ->greeting("Hello {$notifiable->name},")
+        //             ->line('New request for the product ('.$this->request->details->name.') has been created')
+        //             ->action('Show Details', url('admin/request/show',$this->request->id))
+        //             ->line('Thank you for using our application!');
+        // }else{
             $message
             ->subject('New Request')
             ->greeting("Hello {$notifiable->name},")
             ->line('New request for private label has been created')
             ->action('Show Details', url('admin/request/show',$this->request->id))
             ->line('Thank you for using our application!');
-        }
+        // }
         return $message;
     }
 
     public function toDatabase(object $notifiable)
     {
-        if ($this->request->datails->name) {
-            return[
-                'title' => 'New Request',
-                'body' => 'New request for the product ('.$this->request->datails->name.') has been created',
-                'image' => '',
-                'url' => route('admin.request.show',$this->request->id),
-            ];
-        }else{
+        // if ($this->request->datails->name) {
+        //     return[
+        //         'title' => 'New Request',
+        //         'body' => 'New request for the product ('.$this->request->datails->name.') has been created',
+        //         'image' => '',
+        //         'url' => route('admin.request.show',$this->request->id),
+        //     ];
+        // }else{
             return[
                 'title' => 'New Request',
                 'body' => 'New request for private label has been created',
                 'image' => '',
                 'url' => route('admin.request.show',$this->request->id),
             ];
-        }
+        // }
     }
 
     /**
